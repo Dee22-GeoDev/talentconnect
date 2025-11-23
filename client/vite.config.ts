eed665b2660b0fe2628b704c067d39453dc9f8e4
Dee-GeoDev22 <moyadamacline@gmail.com>
2025-11-23 14:23:38 +0300

    updated config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,17 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-  proxy: {
-    "/api": {
-      target: "https://talentconnect-deployment.onrender.com",
-      changeOrigin: true,
+    proxy: {
+      "/api": {
+        target: "https://talentconnect-deployment.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-},
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
